acbca26742e88ead2f603021610d6d2fc1b06ac9
Hieu Nguyen <hieunt.adsagency@gmail.com>
2025-05-15 17:01:47 +0700

    fix: components path

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 import TopBar from "./TopBar";
 
 const Layout = () => {
